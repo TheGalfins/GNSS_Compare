936eb423904c00b30ed476f7cbaee1495fbc5ed3
Mateusz Kraiński <mt.krainski@gmail.com>
2018-10-17 21:40:52 +0200

    Fixed syntax error in conf.py, added authors properly

--- docs/conf.py
+++ docs/conf.py
@@ -139,7 +139,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'GNSSCompare.tex', 'GNSS Compare Documentation',
-     ,'Sebastian Ciuban', 'Mateusz Krainski', 'manual'),
+     'Sebastian Ciuban \\and Mateusz Krainski', 'manual'),
 ]
 
 
